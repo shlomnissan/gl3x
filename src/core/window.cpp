cf69360fc389625eddd27e03866de246f37e634f
Shlomi Nissan <admin@betamark.com>
2024-08-03 16:11:37 -0700

    Initial commit

---
+++ src/core/window.cpp
@@ -0,0 +1,79 @@
+// Copyright 2024 Betamark Pty Ltd. All rights reserved.
+// Author: Shlomi Nissan (shlomi@betamark.com)
+
+#include "window.h"
+
+#include <GLFW/glfw3.h>
+#include <fmt/format.h>
+
+constexpr auto callback_error =
+[](int error, const char* message) {
+    fmt::print("Error ({}): {}\n", error, message);
+};
+
+constexpr auto callback_resize =
+[](GLFWwindow *window, int width, int height) {
+    auto buffer_width {0}, buffer_height {0};
+    glfwGetFramebufferSize(window, &buffer_width, &buffer_height);
+    glViewport(0, 0, buffer_width, buffer_height);
+    auto instance = static_cast<Window*>(glfwGetWindowUserPointer(window));
+    if (instance->on_resize()) {
+        instance->on_resize()(width, height);
+    }
+};
+
+Window::Window(int width, int height, std::string_view title) {
+    glfwSetErrorCallback(callback_error);
+
+    if (!glfwInit()) {
+        return;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_TRUE);
+    #endif
+
+    window_ = glfwCreateWindow(width, height, title.data(), nullptr, nullptr);
+    if (window_ == nullptr) {
+        glfwTerminate();
+        return;
+    }
+
+     glfwMakeContextCurrent(window_);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        return;
+    }
+
+    glfwSwapInterval(1);
+    glfwSetFramebufferSizeCallback(window_, callback_resize);
+    glfwSetWindowUserPointer(window_, this);
+
+    auto buffer_width {0}, buffer_height {0};
+    glfwGetFramebufferSize(window_, &buffer_width, &buffer_height);
+    glViewport(0, 0, buffer_width, buffer_height);
+}
+
+auto Window::Start(const std::function<void(const double delta)> &program) -> void {
+    timer_.Reset();
+
+    while(!glfwWindowShouldClose(window_)) {
+        auto delta = timer_.GetSeconds();
+        timer_.Reset();
+
+        program(delta);
+
+        glfwSwapBuffers(window_);
+        glfwPollEvents();
+    }
+}
+
+Window::~Window() {
+    glfwDestroyWindow(window_);
+    glfwTerminate();
+}
---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+// Copyright 2024 Betamark Pty Ltd. All rights reserved.
+// Author: Shlomi Nissan (shlomi@betamark.com)
+
+#include <array>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "core/mesh.h"
+#include "core/shader.h"
+#include "core/window.h"
+
+#include "shaders/headers/scene_vert.h"
+#include "shaders/headers/scene_frag.h"
+
+#include "mesh/cube.h"
+
+auto main() -> int {
+    const auto width = 800;
+    const auto height = 600;
+    auto window = Window {width, height, "Tetris 3D"};
+
+    auto shader = Shader {{
+        {ShaderType::kVertexShader, _SHADER_scene_vert},
+        {ShaderType::kFragmentShader, _SHADER_scene_frag}
+    }};
+
+    glEnable(GL_DEPTH_TEST);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    auto updateProjection = [&shader](int width, int height) {
+        auto ratio = static_cast<float>(width) / static_cast<float>(height);
+        shader.SetMat4("Projection", glm::perspective(45.0f, ratio, 0.1f, 100.0f));
+    };
+
+    updateProjection(width, height);
+    window.on_resize([&updateProjection](const int width, const int height){
+        updateProjection(width, height);
+    });
+
+    auto view = glm::lookAt(
+        glm::vec3{0.0f, 0.0f, 1.0f},
+        glm::vec3{0.0f, 0.0f, 0.0f},
+        glm::vec3{0.0f, 1.0f, 0.0f}
+    );
+
+    auto cube = Mesh {cube_vertex_0, cube_index_0};
+
+    window.Start([&](const double delta){
+        glClearColor(0.0f, 0.0f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        auto model = glm::mat4{1.0f};
+        model = glm::scale(model, {0.3f, 0.3f, 0.3f});
+        model = glm::rotate(model, static_cast<float>(glfwGetTime()), {1.0f, 1.0f, 1.0f});
+        shader.SetMat4("ModelView", view * model);
+
+        cube.Draw(shader);
+    });
+
+    return 0;
+}
