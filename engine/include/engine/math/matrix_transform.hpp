0a5b540cd0d54c21e9a49b5938918aa119ae3c97
Shlomi Nissan <admin@betamark.com>
2024-08-05 13:53:15 -0700

    Add missing EXPORT statements

--- engine/include/engine/math/matrix_transform.hpp
+++ engine/include/engine/math/matrix_transform.hpp
@@ -10,12 +10,16 @@
 
 namespace engine {
 
+ENGINE_EXPORT
 auto Scale(const Matrix4& m, const Vector3& v) -> Matrix4;
 
+ENGINE_EXPORT
 auto Rotate(const Matrix4& m, float angle, const Vector3& v) -> Matrix4;
 
+ENGINE_EXPORT
 auto Translate(const Matrix4& m, const Vector3& v) -> Matrix4;
 
+ENGINE_EXPORT
 auto LookAt(const Vector3& eye, const Vector3& target, const Vector3& up) -> Matrix4;
 
 }
