2fa5b28b64197cb56c330bc5897ab44a814ed9a9
Shlomi Nissan <admin@betamark.com>
2024-08-07 13:56:14 -0700

    Refactor matrix tests

--- engine/tests/math/matrix3_test.cpp
+++ engine/tests/math/matrix3_test.cpp
@@ -5,68 +5,60 @@
 
 #include <engine/math/matrix3.hpp>
 
-TEST(Matrix3, DefaultConstructor) {
-    auto m = engine::Matrix3 {};
+auto EXPECT_MAT3_EQ(const engine::Matrix3&, const engine::Matrix3&) -> void;
 
-    for (auto i = 0; i < 3; ++i) {
-        for (auto j = 0; j < 3; ++j) {
-            EXPECT_FLOAT_EQ(m(i, j), 0.0f);
-        }
-    }
-}
+TEST(Matrix3, Constructors) {
+    // default constructor
+    auto m1 = engine::Matrix3 {};
+    EXPECT_MAT3_EQ(m1, {
+        0.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.0f
+    });
 
-TEST(Matrix3, SingleParameterConstructor) {
-    auto m = engine::Matrix3 {1.0f};
+    // single parameter (diagonal matrix)
+    auto m2 = engine::Matrix3 {1.0f};
+    EXPECT_MAT3_EQ(m2, {
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f
+    });
 
-    EXPECT_FLOAT_EQ(m(0, 0), 1.0f);
-    EXPECT_FLOAT_EQ(m(0, 1), 0.0f);
-    EXPECT_FLOAT_EQ(m(0, 2), 0.0f);
-
-    EXPECT_FLOAT_EQ(m(1, 0), 0.0f);
-    EXPECT_FLOAT_EQ(m(1, 1), 1.0f);
-    EXPECT_FLOAT_EQ(m(1, 2), 0.0f);
-
-    EXPECT_FLOAT_EQ(m(2, 0), 0.0f);
-    EXPECT_FLOAT_EQ(m(2, 1), 0.0f);
-    EXPECT_FLOAT_EQ(m(2, 2), 1.0f);
-}
-
-TEST(Matrix3, ParameterizedConstructor) {
-    auto m = engine::Matrix3 {
+    // parameterized
+    auto m3 = engine::Matrix3 {
         1.0f, 2.0f, 3.0f,
         4.0f, 5.0f, 6.0f,
-        7.0f, 8.0f, 9.0f,
+        7.0f, 8.0f, 9.0f
     };
+    EXPECT_MAT3_EQ(m3, {
+        1.0f, 2.0f, 3.0f,
+        4.0f, 5.0f, 6.0f,
+        7.0f, 8.0f, 9.0f
+    });
 
-    EXPECT_FLOAT_EQ(m(0, 0), 1.0f);
-    EXPECT_FLOAT_EQ(m(0, 1), 2.0f);
-    EXPECT_FLOAT_EQ(m(0, 2), 3.0f);
-
-    EXPECT_FLOAT_EQ(m(1, 0), 4.0f);
-    EXPECT_FLOAT_EQ(m(1, 1), 5.0f);
-    EXPECT_FLOAT_EQ(m(1, 2), 6.0f);
-
-    EXPECT_FLOAT_EQ(m(2, 0), 7.0f);
-    EXPECT_FLOAT_EQ(m(2, 1), 8.0f);
-    EXPECT_FLOAT_EQ(m(2, 2), 9.0f);
-}
-
-TEST(Matrix3, VectorConstructor) {
-    auto m = engine::Matrix3 {
+    // vector initialization
+    auto m4 = engine::Matrix3 {
         engine::Vector3 {1.0f, 4.0f, 7.0f},
         engine::Vector3 {2.0f, 5.0f, 8.0f},
         engine::Vector3 {3.0f, 6.0f, 9.0f},
     };
+    EXPECT_MAT3_EQ(m4, {
+        1.0f, 2.0f, 3.0f,
+        4.0f, 5.0f, 6.0f,
+        7.0f, 8.0f, 9.0f
+    });
+}
 
-    EXPECT_FLOAT_EQ(m(0, 0), 1.0f);
-    EXPECT_FLOAT_EQ(m(0, 1), 2.0f);
-    EXPECT_FLOAT_EQ(m(0, 2), 3.0f);
+auto EXPECT_MAT3_EQ(const engine::Matrix3& a, const engine::Matrix3& b) -> void {
+    EXPECT_FLOAT_EQ(a(0, 0), b(0, 0));
+    EXPECT_FLOAT_EQ(a(0, 1), b(0, 1));
+    EXPECT_FLOAT_EQ(a(0, 2), b(0, 2));
 
-    EXPECT_FLOAT_EQ(m(1, 0), 4.0f);
-    EXPECT_FLOAT_EQ(m(1, 1), 5.0f);
-    EXPECT_FLOAT_EQ(m(1, 2), 6.0f);
+    EXPECT_FLOAT_EQ(a(1, 0), b(1, 0));
+    EXPECT_FLOAT_EQ(a(1, 1), b(1, 1));
+    EXPECT_FLOAT_EQ(a(1, 2), b(1, 2));
 
-    EXPECT_FLOAT_EQ(m(2, 0), 7.0f);
-    EXPECT_FLOAT_EQ(m(2, 1), 8.0f);
-    EXPECT_FLOAT_EQ(m(2, 2), 9.0f);
+    EXPECT_FLOAT_EQ(a(2, 0), b(2, 0));
+    EXPECT_FLOAT_EQ(a(2, 1), b(2, 1));
+    EXPECT_FLOAT_EQ(a(2, 2), b(2, 2));
 }
--- engine/tests/math/matrix4_test.cpp
+++ engine/tests/math/matrix4_test.cpp
@@ -5,94 +5,74 @@
 
 #include <engine/math/matrix4.hpp>
 
-TEST(Matrix4, DefaultConstructor) {
-    auto m = engine::Matrix4 {};
+auto EXPECT_MAT4_EQ(const engine::Matrix4&, const engine::Matrix4&) -> void;
 
-    for (auto i = 0; i < 4; ++i) {
-        for (auto j = 0; j < 4; ++j) {
-            EXPECT_FLOAT_EQ(m(i, j), 0.0f);
-        }
-    }
-}
+TEST(Matrix4, Constructors) {
+    // default constructor
+    auto m1 = engine::Matrix4 {};
+    EXPECT_MAT4_EQ(m1, {
+        0.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 0.0f
+    });
 
-TEST(Matrix4, SingleParameterConstructor) {
-    auto m = engine::Matrix4 {1.0f};
+    // single parameter (diagonal matrix)
+    auto m2 = engine::Matrix4 {1.0f};
+    EXPECT_MAT4_EQ(m2, {
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f
+    });
 
-    EXPECT_FLOAT_EQ(m(0, 0), 1.0f);
-    EXPECT_FLOAT_EQ(m(0, 1), 0.0f);
-    EXPECT_FLOAT_EQ(m(0, 2), 0.0f);
-    EXPECT_FLOAT_EQ(m(0, 3), 0.0f);
-
-    EXPECT_FLOAT_EQ(m(1, 0), 0.0f);
-    EXPECT_FLOAT_EQ(m(1, 1), 1.0f);
-    EXPECT_FLOAT_EQ(m(1, 2), 0.0f);
-    EXPECT_FLOAT_EQ(m(1, 3), 0.0f);
-
-    EXPECT_FLOAT_EQ(m(2, 0), 0.0f);
-    EXPECT_FLOAT_EQ(m(2, 1), 0.0f);
-    EXPECT_FLOAT_EQ(m(2, 2), 1.0f);
-    EXPECT_FLOAT_EQ(m(2, 3), 0.0f);
-
-    EXPECT_FLOAT_EQ(m(3, 0), 0.0f);
-    EXPECT_FLOAT_EQ(m(3, 1), 0.0f);
-    EXPECT_FLOAT_EQ(m(3, 2), 0.0f);
-    EXPECT_FLOAT_EQ(m(3, 3), 1.0f);
-}
-
-TEST(Matrix4, ParameterizedConstructor) {
-    auto m = engine::Matrix4 {
+    // parameterized
+    auto m3 = engine::Matrix4 {
          1.0f,  2.0f,  3.0f,  4.0f,
          5.0f,  6.0f,  7.0f,  8.0f,
          9.0f, 10.0f, 11.0f, 12.0f,
         13.0f, 14.0f, 15.0f, 16.0f
     };
+    EXPECT_MAT4_EQ(m3, {
+         1.0f,  2.0f,  3.0f,  4.0f,
+         5.0f,  6.0f,  7.0f,  8.0f,
+         9.0f, 10.0f, 11.0f, 12.0f,
+        13.0f, 14.0f, 15.0f, 16.0f
+    });
 
-    EXPECT_FLOAT_EQ(m(0, 0), 1.0f);
-    EXPECT_FLOAT_EQ(m(0, 1), 2.0f);
-    EXPECT_FLOAT_EQ(m(0, 2), 3.0f);
-    EXPECT_FLOAT_EQ(m(0, 3), 4.0f);
-
-    EXPECT_FLOAT_EQ(m(1, 0), 5.0f);
-    EXPECT_FLOAT_EQ(m(1, 1), 6.0f);
-    EXPECT_FLOAT_EQ(m(1, 2), 7.0f);
-    EXPECT_FLOAT_EQ(m(1, 3), 8.0f);
-
-    EXPECT_FLOAT_EQ(m(2, 0), 9.0f);
-    EXPECT_FLOAT_EQ(m(2, 1), 10.0f);
-    EXPECT_FLOAT_EQ(m(2, 2), 11.0f);
-    EXPECT_FLOAT_EQ(m(2, 3), 12.0f);
-
-    EXPECT_FLOAT_EQ(m(3, 0), 13.0f);
-    EXPECT_FLOAT_EQ(m(3, 1), 14.0f);
-    EXPECT_FLOAT_EQ(m(3, 2), 15.0f);
-    EXPECT_FLOAT_EQ(m(3, 3), 16.0f);
-}
-
-TEST(Matrix4, VectorConstructor) {
-    auto m = engine::Matrix4 {
+    // vector initialization
+    auto m4 = engine::Matrix4 {
         engine::Vector4 {1.0f, 5.0f,  9.0f, 13.0f},
         engine::Vector4 {2.0f, 6.0f, 10.0f, 14.0f},
         engine::Vector4 {3.0f, 7.0f, 11.0f, 15.0f},
         engine::Vector4 {4.0f, 8.0f, 12.0f, 16.0f}
     };
+    EXPECT_MAT4_EQ(m4, {
+         1.0f,  2.0f,  3.0f,  4.0f,
+         5.0f,  6.0f,  7.0f,  8.0f,
+         9.0f, 10.0f, 11.0f, 12.0f,
+        13.0f, 14.0f, 15.0f, 16.0f
+    });
+}
 
-    EXPECT_FLOAT_EQ(m(0, 0), 1.0f);
-    EXPECT_FLOAT_EQ(m(0, 1), 2.0f);
-    EXPECT_FLOAT_EQ(m(0, 2), 3.0f);
-    EXPECT_FLOAT_EQ(m(0, 3), 4.0f);
+auto EXPECT_MAT4_EQ(const engine::Matrix4& a, const engine::Matrix4& b) -> void {
+    EXPECT_FLOAT_EQ(a(0, 0), b(0, 0));
+    EXPECT_FLOAT_EQ(a(0, 1), b(0, 1));
+    EXPECT_FLOAT_EQ(a(0, 2), b(0, 2));
+    EXPECT_FLOAT_EQ(a(0, 3), b(0, 3));
 
-    EXPECT_FLOAT_EQ(m(1, 0), 5.0f);
-    EXPECT_FLOAT_EQ(m(1, 1), 6.0f);
-    EXPECT_FLOAT_EQ(m(1, 2), 7.0f);
-    EXPECT_FLOAT_EQ(m(1, 3), 8.0f);
+    EXPECT_FLOAT_EQ(a(1, 0), b(1, 0));
+    EXPECT_FLOAT_EQ(a(1, 1), b(1, 1));
+    EXPECT_FLOAT_EQ(a(1, 2), b(1, 2));
+    EXPECT_FLOAT_EQ(a(1, 3), b(1, 3));
 
-    EXPECT_FLOAT_EQ(m(2, 0), 9.0f);
-    EXPECT_FLOAT_EQ(m(2, 1), 10.0f);
-    EXPECT_FLOAT_EQ(m(2, 2), 11.0f);
-    EXPECT_FLOAT_EQ(m(2, 3), 12.0f);
+    EXPECT_FLOAT_EQ(a(2, 0), b(2, 0));
+    EXPECT_FLOAT_EQ(a(2, 1), b(2, 1));
+    EXPECT_FLOAT_EQ(a(2, 2), b(2, 2));
+    EXPECT_FLOAT_EQ(a(2, 3), b(2, 3));
 
-    EXPECT_FLOAT_EQ(m(3, 0), 13.0f);
-    EXPECT_FLOAT_EQ(m(3, 1), 14.0f);
-    EXPECT_FLOAT_EQ(m(3, 2), 15.0f);
-    EXPECT_FLOAT_EQ(m(3, 3), 16.0f);
+    EXPECT_FLOAT_EQ(a(3, 0), b(3, 0));
+    EXPECT_FLOAT_EQ(a(3, 1), b(3, 1));
+    EXPECT_FLOAT_EQ(a(3, 2), b(3, 2));
+    EXPECT_FLOAT_EQ(a(3, 3), b(3, 3));
 }
