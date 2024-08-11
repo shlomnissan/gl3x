bf2a1b1c066a16c70d992ec3850eca7ace3f2506
Shlomi Nissan <admin@betamark.com>
2024-08-11 12:57:06 -0700

    Add matrix to matrix multiplication

--- engine/tests/math/matrix3_test.cpp
+++ engine/tests/math/matrix3_test.cpp
@@ -47,3 +47,22 @@ TEST(Matrix3, Constructors) {
         7.0f, 8.0f, 9.0f
     });
 }
+
+TEST(Matrix3, MatrixMultiplication) {
+    auto m1 = engine::Matrix3 {
+        1.0f, 2.0f, 3.0f,
+        5.0f, 6.0f, 7.0f,
+        4.0f, 3.0f, 2.0f
+    };
+    auto m2 = engine::Matrix3 {
+        1.0f, 5.0f, 1.0f,
+        2.0f, 1.0f, 3.0f,
+        1.0f, 5.0f, 4.0f
+    };
+
+    EXPECT_MAT3_EQ((m1 * m2), {
+         8.0f, 22.0f, 19.0f,
+        24.0f, 66.0f, 51.0f,
+        12.0f, 33.0f, 21.0f,
+    });
+}
--- engine/tests/math/matrix4_test.cpp
+++ engine/tests/math/matrix4_test.cpp
@@ -53,3 +53,25 @@ TEST(Matrix4, Constructors) {
         13.0f, 14.0f, 15.0f, 16.0f
     });
 }
+
+TEST(Matrix4, MatrixMultiplication) {
+    auto m1 = engine::Matrix4 {
+        1.0f, 2.0f, 3.0f, 4.0f,
+        5.0f, 6.0f, 7.0f, 8.0f,
+        4.0f, 3.0f, 2.0f, 1.0f,
+        8.0f, 7.0f, 6.0f, 5.0f,
+    };
+    auto m2 = engine::Matrix4 {
+        1.0f, 5.0f, 1.0f, 8.0f,
+        2.0f, 1.0f, 3.0f, 1.0f,
+        1.0f, 5.0f, 4.0f, 2.0f,
+        9.0f, 6.0f, 1.0f, 3.0f
+    };
+
+    EXPECT_MAT4_EQ((m1 * m2), {
+        44.0f,  46.0f, 23.0f, 28.0f,
+        96.0f, 114.0f, 59.0f, 84.0f,
+        21.0f,  39.0f, 22.0f, 42.0f,
+        73.0f, 107.0f, 58.0f, 98.0f
+    });
+}
