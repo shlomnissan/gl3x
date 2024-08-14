ef691a3d5c4903bfca85259a4ceaae7eb9238fc9
Shlomi Nissan <admin@betamark.com>
2024-08-14 09:08:02 -0700

    Add rotation transform

--- engine/src/math/matrix_transform.cpp
+++ engine/src/math/matrix_transform.cpp
@@ -3,13 +3,15 @@
 
 #include <engine/math/matrix_transform.hpp>
 
+#include <cmath>
+
 namespace engine {
 
 auto scale(const Matrix4& m, const Vector3& v) -> Matrix4 {
     auto output = Matrix4 {};
-    output[0] = m[0] * v.x;
-    output[1] = m[1] * v.y;
-    output[2] = m[2] * v.z;
+    output[0] = m[0] * v[0];
+    output[1] = m[1] * v[1];
+    output[2] = m[2] * v[2];
     output[3] = m[3];
     return output;
 }
@@ -19,13 +21,43 @@ auto scale(const Matrix4& m, const float value) -> Matrix4 {
 }
 
 auto rotate(const Matrix4& m, float angle, const Vector3& v) -> Matrix4 {
-    // TODO: impl.
-    return {};
+    const auto a = angle;
+    const auto c = std::cos(a);
+    const auto s = std::sin(a);
+    const auto axis = normalize(v);
+    const auto temp = (1.0f - c) * axis;
+
+    auto rotate = engine::Matrix4 {};
+    rotate[0] = {
+        c + temp[0] * axis[0],
+        temp[0] * axis[1] + s * axis[2],
+        temp[0] * axis[2] - s * axis[1],
+        0.0f
+    };
+    rotate[1] = {
+        temp[1] * axis[0] - s * axis[2],
+        c + temp[1] * axis[1],
+        temp[1] * axis[2] + s * axis[0],
+        0.0f
+    };
+    rotate[2] = {
+        temp[2] * axis[0] + s * axis[1],
+        temp[2] * axis[1] - s * axis[0],
+        c + temp[2] * axis[2],
+        0.0f
+    };
+
+    auto output = engine::Matrix4 {};
+    output[0] = m[0] * rotate[0][0] + m[1] * rotate[0][1] + m[2] * rotate[0][2];
+    output[1] = m[0] * rotate[1][0] + m[1] * rotate[1][1] + m[2] * rotate[1][2];
+    output[2] = m[0] * rotate[2][0] + m[1] * rotate[2][1] + m[2] * rotate[2][2];
+    output[3] = m[3];
+    return output;
 }
 
 auto translate(const Matrix4& m, const Vector3& v) -> Matrix4 {
     auto output = Matrix4 {m};
-    output[3] = m[0] * v.x + m[1] * v.y + m[2] * v.z + m[3];
+    output[3] = m[0] * v[0] + m[1] * v[1] + m[2] * v[2] + m[3];
     return output;
 }
 
