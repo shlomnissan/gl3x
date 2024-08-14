6cbf6660b92248a025ce02d0d97ed95fc15b60e7
Shlomi Nissan <admin@betamark.com>
2024-08-14 08:57:05 -0700

    Add perspective projection

--- engine/src/math/projection.cpp
+++ engine/src/math/projection.cpp
@@ -3,6 +3,8 @@
 
 #include <engine/math/projection.hpp>
 
+#include <cmath>
+
 namespace engine {
 
 auto ortho(float left, float right, float bottom, float top, float near, float far) -> Matrix4 {
@@ -20,8 +22,12 @@ auto ortho(float left, float right, float bottom, float top, float near, float f
 }
 
 auto perspective(float fov, float aspect_ratio, float near, float far) -> Matrix4 {
-    // TODO: implement
-    auto output = Matrix4 {};
+    const auto tan_half_fov = std::tan(fov / 2);
+    auto output = Matrix4 {0.0f};
+    output[0] = {1 / (aspect_ratio * tan_half_fov), 0.0f, 0.0f, 0.0f};
+    output[1] = {0.0f, 1 / tan_half_fov, 0.0f, 0.0f};
+    output[2] = {0.0f, 0.0f, -(far + near) / (far - near), -1.0f};
+    output[3] = {0.0f, 0.0f, -(2 * far * near) / (far - near), 0.0f};
     return output;
 }
 
--- engine/tests/math/projection_test.cpp
+++ engine/tests/math/projection_test.cpp
@@ -4,33 +4,82 @@
 #include <gtest/gtest.h>
 #include <test_helpers.hpp>
 
+#include <numbers>
+
 #include <engine/math/projection.hpp>
 
-TEST(Projection, Orthographic) {
-    // standard viewport
-    const auto m1 = engine::ortho(-1.0f, 1.0f, -1.0f, 1.0f, -2.0f, 0.0f);
-    EXPECT_MAT4_EQ(m1, {
+constexpr auto pi_4 = std::numbers::pi / 4;
+
+#pragma region Orthographic Projection
+
+TEST(Projection, OrthographicStandardViewport) {
+    const auto m = engine::ortho(-1.0f, 1.0f, -1.0f, 1.0f, -2.0f, 0.0f);
+
+    EXPECT_MAT4_EQ(m, {
         1.0f, 0.0f,  0.0f, 0.0f,
         0.0f, 1.0f,  0.0f, 0.0f,
         0.0f, 0.0f, -1.0f, 1.0f,
         0.0f, 0.0f,  0.0f, 1.0f
     });
+}
 
-    // screen space
-    const auto m2 = engine::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f);
-    EXPECT_MAT4_NEAR(m2, {
+TEST(Projection, OrthographicScreenSpace) {
+    const auto m = engine::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f);
+
+    EXPECT_MAT4_NEAR(m, {
         0.0024f, 0.0000f,  0.0000f, -1.0000f,
         0.0000f, 0.0033f,  0.0000f, -1.0000f,
         0.0000f, 0.0000f, -0.0200f, -1.0020f,
         0.0000f, 0.0000f,  0.0000f,  1.0000f
     }, 0.001f);
+}
 
-    // positive near-far planes
-    const auto m3 = engine::ortho(-1.0f, 1.0f, -1.0f, 1.0f, 0.5f, 1.5f);
-    EXPECT_MAT4_EQ(m3, {
+TEST(Projection, OrthographicPositiveNearFarPlanes) {
+    const auto m = engine::ortho(-1.0f, 1.0f, -1.0f, 1.0f, 0.5f, 1.5f);
+
+    EXPECT_MAT4_EQ(m, {
         1.0f, 0.0f,  0.0f,  0.0f,
         0.0f, 1.0f,  0.0f,  0.0f,
         0.0f, 0.0f, -2.0f, -2.0f,
         0.0f, 0.0f,  0.0f,  1.0f
     });
 }
+
+#pragma endregion
+
+#pragma region Perspective Projection
+
+TEST(Projection, PerspectiveStandardViewport) {
+    const auto m = engine::perspective(pi_4, 1.0f, 0.1f, 100.0f);
+
+    EXPECT_MAT4_NEAR(m, {
+        2.4142f, 0.0000f,  0.0000f,  0.0000f,
+        0.0000f, 2.4142f,  0.0000f,  0.0000f,
+        0.0000f, 0.0000f, -1.0020f, -0.2002f,
+        0.0000f, 0.0000f, -1.0000f,  0.0000f
+    }, 0.001f);
+}
+
+TEST(Projection, PerspectiveWideAspectRatio) {
+    const auto m = engine::perspective(pi_4, 2.0f, 0.1f, 100.0f);
+
+    EXPECT_MAT4_NEAR(m, {
+        1.2071f, 0.0000f,  0.0000f,  0.0000f,
+        0.0000f, 2.4142f,  0.0000f,  0.0000f,
+        0.0000f, 0.0000f, -1.0020f, -0.2002f,
+        0.0000f, 0.0000f, -1.0000f,  0.0000f
+    }, 0.001f);
+}
+
+TEST(Projection, PerspectiveTallAspectRatio) {
+    const auto m = engine::perspective(pi_4, 0.5f, 0.1f, 100.0f);
+
+    EXPECT_MAT4_NEAR(m, {
+        4.8284f, 0.0000f,  0.0000f,  0.0000f,
+        0.0000f, 2.4142f,  0.0000f,  0.0000f,
+        0.0000f, 0.0000f, -1.0020f, -0.2002f,
+        0.0000f, 0.0000f, -1.0000f,  0.0000f
+    }, 0.001f);
+}
+
+#pragma endregion
