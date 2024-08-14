b4b07f23f3844a26f2aa9de89e4bd31715fb07cd
Shlomi Nissan <admin@betamark.com>
2024-08-14 16:02:39 -0700

    Refactor matrix test files

--- engine/tests/math/matrix3_test.cpp
+++ engine/tests/math/matrix3_test.cpp
@@ -6,56 +6,67 @@
 
 #include <engine/math/matrix3.hpp>
 
-TEST(Matrix3, Constructors) {
-    // default constructor
-    auto m1 = engine::Matrix3 {};
-    EXPECT_MAT3_EQ(m1, {
+#pragma region Constructors
+
+TEST(Matrix3, ConstructorDefault) {
+    const auto m = engine::Matrix3 {};
+
+    EXPECT_MAT3_EQ(m, {
         0.0f, 0.0f, 0.0f,
         0.0f, 0.0f, 0.0f,
         0.0f, 0.0f, 0.0f
     });
+}
 
-    // single parameter (diagonal matrix)
-    auto m2 = engine::Matrix3 {1.0f};
-    EXPECT_MAT3_EQ(m2, {
+TEST(Matrix3, ConstructorSingleParameter) {
+    const auto m = engine::Matrix3 {1.0f};
+
+    EXPECT_MAT3_EQ(m, {
         1.0f, 0.0f, 0.0f,
         0.0f, 1.0f, 0.0f,
         0.0f, 0.0f, 1.0f
     });
+}
 
-    // parameterized
-    auto m3 = engine::Matrix3 {
+TEST(Matrix3, ConstructorParameterized) {
+    const auto m = engine::Matrix3 {
         1.0f, 2.0f, 3.0f,
         4.0f, 5.0f, 6.0f,
         7.0f, 8.0f, 9.0f
     };
-    EXPECT_MAT3_EQ(m3, {
-        1.0f, 2.0f, 3.0f,
-        4.0f, 5.0f, 6.0f,
-        7.0f, 8.0f, 9.0f
-    });
 
-    // vector initialization
-    auto m4 = engine::Matrix3 {
-        engine::Vector3 {1.0f, 4.0f, 7.0f},
-        engine::Vector3 {2.0f, 5.0f, 8.0f},
-        engine::Vector3 {3.0f, 6.0f, 9.0f},
-    };
-    EXPECT_MAT3_EQ(m4, {
+    EXPECT_MAT3_EQ(m, {
         1.0f, 2.0f, 3.0f,
         4.0f, 5.0f, 6.0f,
         7.0f, 8.0f, 9.0f
     });
 }
 
-TEST(Matrix3, MatrixMultiplication) {
-    auto m1 = engine::Matrix3 {
+TEST(Matrix3, ConstructorVector) {
+    const auto m = engine::Matrix3 {
+        engine::Vector3 {1.0f, 4.0f, 7.0f},
+        engine::Vector3 {2.0f, 5.0f, 8.0f},
+        engine::Vector3 {3.0f, 6.0f, 9.0f},
+    };
+
+    EXPECT_MAT3_EQ(m, {
+        1.0f, 2.0f, 3.0f,
+        4.0f, 5.0f, 6.0f,
+        7.0f, 8.0f, 9.0f
+    });
+}
+
+#pragma endregion
+
+#pragma region Matrix/Matrix Multiplication
+
+TEST(Matrix3, MultiplicationMatrix) {
+    const auto m1 = engine::Matrix3 {
         1.0f, 2.0f, 3.0f,
         5.0f, 6.0f, 7.0f,
         4.0f, 3.0f, 2.0f
     };
-
-    auto m2 = engine::Matrix3 {
+    const auto m2 = engine::Matrix3 {
         1.0f, 5.0f, 1.0f,
         2.0f, 1.0f, 3.0f,
         1.0f, 5.0f, 4.0f
@@ -67,3 +78,5 @@ TEST(Matrix3, MatrixMultiplication) {
         12.0f, 33.0f, 21.0f,
     });
 }
+
+#pragma endregion
--- engine/tests/math/matrix4_test.cpp
+++ engine/tests/math/matrix4_test.cpp
@@ -6,47 +6,39 @@
 
 #include <engine/math/matrix4.hpp>
 
-TEST(Matrix4, Constructors) {
-    // default constructor
-    auto m1 = engine::Matrix4 {};
-    EXPECT_MAT4_EQ(m1, {
+#pragma region Constructors
+
+TEST(Matrix4, ConstructorDefault) {
+    const auto m = engine::Matrix4 {};
+
+    EXPECT_MAT4_EQ(m, {
         0.0f, 0.0f, 0.0f, 0.0f,
         0.0f, 0.0f, 0.0f, 0.0f,
         0.0f, 0.0f, 0.0f, 0.0f,
         0.0f, 0.0f, 0.0f, 0.0f
     });
+}
 
-    // single parameter (diagonal matrix)
-    auto m2 = engine::Matrix4 {1.0f};
-    EXPECT_MAT4_EQ(m2, {
+TEST(Matrix4, ConstructorSingleParameter) {
+    const auto m = engine::Matrix4 {1.0f};
+
+    EXPECT_MAT4_EQ(m, {
         1.0f, 0.0f, 0.0f, 0.0f,
         0.0f, 1.0f, 0.0f, 0.0f,
         0.0f, 0.0f, 1.0f, 0.0f,
         0.0f, 0.0f, 0.0f, 1.0f
     });
+}
 
-    // parameterized
-    auto m3 = engine::Matrix4 {
+TEST(Matrix4, ConstructorParameterized) {
+    const auto m = engine::Matrix4 {
          1.0f,  2.0f,  3.0f,  4.0f,
          5.0f,  6.0f,  7.0f,  8.0f,
          9.0f, 10.0f, 11.0f, 12.0f,
         13.0f, 14.0f, 15.0f, 16.0f
     };
-    EXPECT_MAT4_EQ(m3, {
-         1.0f,  2.0f,  3.0f,  4.0f,
-         5.0f,  6.0f,  7.0f,  8.0f,
-         9.0f, 10.0f, 11.0f, 12.0f,
-        13.0f, 14.0f, 15.0f, 16.0f
-    });
 
-    // vector initialization
-    auto m4 = engine::Matrix4 {
-        engine::Vector4 {1.0f, 5.0f,  9.0f, 13.0f},
-        engine::Vector4 {2.0f, 6.0f, 10.0f, 14.0f},
-        engine::Vector4 {3.0f, 7.0f, 11.0f, 15.0f},
-        engine::Vector4 {4.0f, 8.0f, 12.0f, 16.0f}
-    };
-    EXPECT_MAT4_EQ(m4, {
+    EXPECT_MAT4_EQ(m, {
          1.0f,  2.0f,  3.0f,  4.0f,
          5.0f,  6.0f,  7.0f,  8.0f,
          9.0f, 10.0f, 11.0f, 12.0f,
@@ -54,15 +46,34 @@ TEST(Matrix4, Constructors) {
     });
 }
 
-TEST(Matrix4, MatrixMultiplication) {
-    auto m1 = engine::Matrix4 {
+TEST(Matrix4, ConstructorVector) {
+    const auto m = engine::Matrix4 {
+        engine::Vector4 {1.0f, 5.0f,  9.0f, 13.0f},
+        engine::Vector4 {2.0f, 6.0f, 10.0f, 14.0f},
+        engine::Vector4 {3.0f, 7.0f, 11.0f, 15.0f},
+        engine::Vector4 {4.0f, 8.0f, 12.0f, 16.0f}
+    };
+
+    EXPECT_MAT4_EQ(m, {
+         1.0f,  2.0f,  3.0f,  4.0f,
+         5.0f,  6.0f,  7.0f,  8.0f,
+         9.0f, 10.0f, 11.0f, 12.0f,
+        13.0f, 14.0f, 15.0f, 16.0f
+    });
+}
+
+#pragma endregion
+
+#pragma region Matrix/Matrix Multiplication
+
+TEST(Matrix4, MultiplicationMatrix) {
+    const auto m1 = engine::Matrix4 {
         1.0f, 2.0f, 3.0f, 4.0f,
         5.0f, 6.0f, 7.0f, 8.0f,
         4.0f, 3.0f, 2.0f, 1.0f,
         8.0f, 7.0f, 6.0f, 5.0f,
     };
-
-    auto m2 = engine::Matrix4 {
+    const auto m2 = engine::Matrix4 {
         1.0f, 5.0f, 1.0f, 8.0f,
         2.0f, 1.0f, 3.0f, 1.0f,
         1.0f, 5.0f, 4.0f, 2.0f,
@@ -76,3 +87,5 @@ TEST(Matrix4, MatrixMultiplication) {
         73.0f, 107.0f, 58.0f, 98.0f
     });
 }
+
+#pragma endregion
