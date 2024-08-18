b39d225a67ca57ad525bcef816d456953dcb1dc1
Shlomi Nissan <shlomi@betamark.com>
2024-08-18 12:37:14 -0700

    Add initial window implementation using the pimpl idiom

--- include/engine/core/window.hpp
+++ include/engine/core/window.hpp
@@ -5,25 +5,49 @@
 
 #include "engine_export.h"
 
-#include <functional>
+#include <memory>
 #include <string>
 #include <string_view>
 
 namespace engine {
 
+/**
+ * @brief General interface for creating and managing a window.
+ */
 class ENGINE_EXPORT Window {
 public:
-    explicit Window(std::string_view title);
+    /**
+    * @brief Constructs a Window with the given title, width, and height.
+    * 
+    * This constructor initializes a window with the specified title, width, and height. 
+    * If width and height are not provided, they default to 800 and 600 pixels respectively.
+    * 
+    * @param title The title of the window.
+    * @param width The width of the window in pixels. Defaults to 800.
+    * @param height The height of the window in pixels. Defaults to 600.
+    */
+    Window(std::string_view title, int width = 800, int height = 600);
 
-    Window(std::string_view title, int width, int height);
+    /**
+     * @brief Gets the width of the window.
+     * 
+     * @return The width of the window in pixels.
+     */
+    [[nodiscard]] auto width() const -> int;
 
-    auto Tick(const std::function<void(const double delta)>& tick) -> void;
+    /**
+     * @brief Gets the height of the window.
+     * 
+     * @return The height of the window in pixels.
+     */
+    [[nodiscard]] auto height() const -> int;
 
 private:
-    int width_ = 800;
-    int height_ = 600;
+    /// @brief Forward declaration of the implementation class.
+    class Impl;
 
-    std::string title_;
+    /// @brief Pointer to the implementation class.
+    std::unique_ptr<Impl> impl_;
 };
 
 }
--- src/core/window.cpp
+++ src/core/window.cpp
@@ -3,15 +3,71 @@
 
 #include <engine/core/window.hpp>
 
+#include <string_view>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 namespace engine {
 
-Window::Window(std::string_view title) : title_(title) {}
+#pragma region implementation
+
+class Window::Impl {
+public:
+    auto Initialize(int width, int height, std::string_view title) {
+        if (!glfwInit()) return;
+
+        initialized_ = true;
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, 1);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        window_ = glfwCreateWindow(width, height, title.data(), nullptr, nullptr);
+        if (window_ == nullptr) return;
+
+        glfwMakeContextCurrent(window_);
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+            return;
+        }
+
+        glfwSwapInterval(1);
+        glfwGetFramebufferSize(window_, &buffer_width_, &buffer_height_);
+    }
+
+    auto BufferWidth() const { return buffer_width_; }
+
+    auto BufferHeight() const { return buffer_height_; }
+
+    ~Impl() {
+        if (window_) glfwDestroyWindow(window_);
+        if (initialized_) glfwTerminate();
+    }
+
+private: 
+    bool initialized_ {false};
+    int buffer_width_ {0};
+    int buffer_height_ {0};
+
+    GLFWwindow* window_ {nullptr};
+};
+
+#pragma endregion
+
+#pragma region delegate
 
 Window::Window(std::string_view title, int width, int height)
-    : width_(width), height_(height), title_(title) {}
-
-auto Window::Tick(const std::function<void (const double)> &tick) -> void {
-    // TODO: impl
+  : impl_(std::make_unique<Impl>())
+{
+    impl_->Initialize(width, height, title);
 }
+
+auto Window::width() const -> int { return impl_->BufferWidth(); }
+
+auto Window::height() const -> int { return impl_->BufferHeight(); }
+
+#pragma endregion
 
 }
