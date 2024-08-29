eaf3f31a4aae4e2386f111835b39ea81a665801c
Shlomi Nissan <admin@betamark.com>
2024-08-29 11:27:02 -0700

    Simplify the example application

--- example/main.cpp
+++ example/main.cpp
@@ -1,8 +1,6 @@
 // Copyright 2024 Betamark Pty Ltd. All rights reserved.
 // Author: Shlomi Nissan (shlomi@betamark.com)
 
-#include <array>
-
 #include <engine/core/window.hpp>
 #include <engine/math/utilities.hpp>
 #include <engine/scene/camera_perspective.hpp>
@@ -20,13 +18,8 @@ auto main() -> int {
 
     auto scene = engine::Scene::Create();
     auto node = engine::Node::Create();
-    node->Scale(0.5f);
     scene->Add(node);
-
-    auto child = engine::Node::Create();
-    child->Scale(0.6f);
-    child->TranslateX(0.5f);
-    node->Add(child);
+    camera->TranslateZ(1.0f);
 
     window.Start([&](const double _){
         node->RotateX(0.01f);
