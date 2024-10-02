34e6fc0db10ef9d04bf9b0cfb03c51101ef9f034
Shlomi Nissan <admin@betamark.com>
2024-10-02 08:13:19 -0700

    Add a simple RGBA color class

---
+++ include/engine/math/color.hpp
@@ -0,0 +1,52 @@
+// Copyright 2024 Betamark Pty Ltd. All rights reserved.
+// Author: Shlomi Nissan (shlomi@betamark.com)
+
+#pragma once
+
+#include "engine_export.h"
+
+namespace engine {
+
+/**
+ * @brief A color class representing RGBA color values.
+ */
+class ENGINE_EXPORT Color  {
+public:
+    /// @brief The red component.
+    float r {0.0f};
+    /// @brief The green component.
+    float g {0.0f};
+    /// @brief The blue component.
+    float b {0.0f};
+    /// @brief The alpha (transparency) component.
+    float a {1.0f};
+
+    /**
+     * @brief Default constructor that initializes the color to black with full opacity.
+     */
+    Color() = default;
+
+    /**
+     * @brief Constructs a color with the specified red, green, and blue components.
+     * The alpha component is set to 1.0 by default.
+     *
+     * @param r The red component of the color.
+     * @param g The green component of the color.
+     * @param b The blue component of the color.
+     */
+    Color(float r, float g, float b)
+        : r(r), g(g), b(b) {}
+
+    /**
+     * @brief Constructs a color with the specified red, green, blue, and alpha components.
+     *
+     * @param r The red component of the color.
+     * @param g The green component of the color.
+     * @param b The blue component of the color.
+     * @param a The alpha (transparency) component of the color.
+     */
+    Color(float r, float g, float b, float a)
+        : r(r), g(g), b(b), a(a) {}
+};
+
+}
---
+++ tests/math/color_test.cpp
@@ -0,0 +1,37 @@
+// Copyright 2024 Betamark Pty Ltd. All rights reserved.
+// Author: Shlomi Nissan (shlomi@betamark.com)
+
+#include <gtest/gtest.h>
+
+#include <engine/math/color.hpp>
+
+#pragma region Constructors
+
+TEST(Color, ConstructorDefault) {
+    auto color = engine::Color {};
+
+    EXPECT_FLOAT_EQ(color.r, 0.0f);
+    EXPECT_FLOAT_EQ(color.g, 0.0f);
+    EXPECT_FLOAT_EQ(color.b, 0.0f);
+    EXPECT_FLOAT_EQ(color.a, 1.0f);
+}
+
+TEST(Color, ConstructorRGB) {
+    auto color = engine::Color {0.5f, 0.25f, 0.75f};
+
+    EXPECT_FLOAT_EQ(color.r, 0.5f);
+    EXPECT_FLOAT_EQ(color.g, 0.25f);
+    EXPECT_FLOAT_EQ(color.b, 0.75f);
+    EXPECT_FLOAT_EQ(color.a, 1.0f);
+}
+
+TEST(Color, ConstructorRGBA) {
+    auto color = engine::Color {0.3f, 0.6f, 0.9f, 0.5f};
+
+    EXPECT_FLOAT_EQ(color.r, 0.3f);
+    EXPECT_FLOAT_EQ(color.g, 0.6f);
+    EXPECT_FLOAT_EQ(color.b, 0.9f);
+    EXPECT_FLOAT_EQ(color.a, 0.5f);
+}
+
+#pragma endregion
