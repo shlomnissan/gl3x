9b9bd1515991e2b6947a200e7903bfd3768dd254
Shlomi Nissan <admin@betamark.com>
2024-10-06 07:47:51 -0700

    Add Phong material placeholder

---
+++ include/engine/materials/phong_material.hpp
@@ -0,0 +1,58 @@
+// Copyright 2024 Betamark Pty Ltd. All rights reserved.
+// Author: Shlomi Nissan (shlomi@betamark.com)
+
+#pragma once
+
+#include "engine_export.h"
+#include "engine/math/color.hpp"
+
+#include "materials/material.hpp"
+#include "textures/texture.hpp"
+
+#include <memory>
+
+namespace engine {
+
+/**
+ * @brief A material for drawing geometries using the non-physically based
+ * Blinn-Phong model for calculating reflectance.
+ */
+class ENGINE_EXPORT PhongMaterial : public Material {
+public:
+    /**
+     * @brief Structure to hold the parameters for the PhongMaterial.
+     */
+    struct Parameters {
+        /// @brief The base color of the material.
+        Color color {};
+        /// @brief Optional texture to be applied to the material.
+        std::shared_ptr<Texture> texture {nullptr};
+    };
+
+    /**
+     * @brief Constructs a PhongMaterial with the provided parameters.
+     *
+     * @param params A Parameters struct containing the color and optional texture.
+     */
+    explicit PhongMaterial(const Parameters& params) : params_(params) {}
+
+    auto Type() const -> std::string override {
+        return "phong_material";
+    }
+
+    /**
+     * @brief Creates a shared pointer to a default-constructed object.
+     *
+     * @param params A Parameters struct containing the color and optional texture.
+     * @return std::shared_ptr<PhongMaterial> A shared pointer to the newly created object.
+     */
+    [[nodiscard]] static auto Create(const Parameters& params) {
+        return std::make_shared<PhongMaterial>(params);
+    }
+
+private:
+    /// @brief The parameters of the material, including color and optional texture.
+    Parameters params_;
+};
+
+}
