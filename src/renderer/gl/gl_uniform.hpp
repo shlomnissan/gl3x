a5ad381472731451d0de52d716491a0cdf8178be
Shlomi Nissan <admin@betamark.com>
2024-10-10 16:51:53 -0700

    Add OpenGL uniform type

---
+++ src/renderer/gl/gl_uniform.hpp
@@ -0,0 +1,25 @@
+// Copyright 2024 Betamark Pty Ltd. All rights reserved.
+// Author: Shlomi Nissan (shlomi@betamark.com)
+
+#pragma once
+
+#include "engine/math/color.hpp"
+#include "engine/math/matrix4.hpp"
+
+#include <variant>
+
+#include <glad/glad.h>
+
+namespace engine {
+
+using GLUniformValue = std::variant<float, Color, Matrix4>;
+
+struct GLUniform {
+    GLint location {0};
+    GLint size {0};
+    GLenum type {0};
+    GLUniformValue value {0};
+    bool needs_update {false};
+};
+
+}
