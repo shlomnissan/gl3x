96c3a534c0b9e6e1403214548ff86fcb8b73dcc4
Shlomi Nissan <shlomi@betamark.com>
2024-10-20 10:54:09 -0700

    Flip the orthographic projection vertically

--- include/engine/scene/camera_orthographic.hpp
+++ include/engine/scene/camera_orthographic.hpp
@@ -23,16 +23,16 @@ public:
      *
      * @param left The left bound of the view volume.
      * @param right The right bound of the view volume.
-     * @param bottom The bottom bound of the view volume.
      * @param top The top bound of the view volume.
+     * @param bottom The bottom bound of the view volume.
      * @param near The distance to the near clipping plane.
      * @param far The distance to the far clipping plane.
      */
     CameraOrthographic(
         float left,
         float right,
-        float bottom,
         float top,
+        float bottom,
         float near,
         float far
     );
@@ -42,8 +42,8 @@ public:
      *
      * @param left The left bound of the orthographic view volume (default is -1.0f).
      * @param right The right bound of the orthographic view volume (default is 1.0f).
-     * @param bottom The bottom bound of the orthographic view volume (default is -1.0f).
      * @param top The top bound of the orthographic view volume (default is 1.0f).
+     * @param bottom The bottom bound of the orthographic view volume (default is -1.0f).
      * @param near The distance to the near clipping plane (default is 0.1f).
      * @param far The distance to the far clipping plane (default is 100.0f).
      * @return A `std::shared_ptr<CameraOrthographic>` pointing to the newly created instance.
@@ -52,8 +52,8 @@ public:
     static auto Create(
         float left = -1.0f,
         float right = 1.0f,
-        float bottom = -1.0f,
         float top = 1.0f,
+        float bottom = -1.0f,
         float near = 0.1f,
         float far = 100.0f
     ) {
@@ -67,10 +67,10 @@ private:
     float left_;
     /// @brief The right bound of the orthographic view volume.
     float right_;
-    /// @brief The bottom bound of the orthographic view volume.
-    float bottom_;
     /// @brief The top bound of the orthographic view volume.
     float top_;
+    /// @brief The bottom bound of the orthographic view volume.
+    float bottom_;
 
     /**
      * @brief Updates the projection matrix for the orthographic camera.
