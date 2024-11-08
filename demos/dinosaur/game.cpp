9f992666397af9ccd31b21c0a2643970483764c6
Shlomi Nissan <admin@betamark.com>
2024-11-08 10:09:09 -0800

    Remove the demos directory

--- demos/dinosaur/game.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2024 Betamark Pty Ltd. All rights reserved.
-// Author: Shlomi Nissan (shlomi@betamark.com)
-
-#include "game.hpp"
-
-Game::Game() {
-    Add(dinosaur_);
-    Add(ground_);
-}
