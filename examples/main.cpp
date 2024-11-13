2f794870762d3568517bffdf2013b6a1ced85702
Shlomi Nissan <shlomi@betamark.com>
2024-11-13 08:40:59 -0800

    Reset the main example

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,7 +1,6 @@
 // Copyright 2024 Betamark Pty Ltd. All rights reserved.
 // Author: Shlomi Nissan (shlomi@betamark.com)
 
-#include "materials/material.hpp"
 #include <engine/core/renderer.hpp>
 #include <engine/core/window.hpp>
 #include <engine/geometry/box_geometry.hpp>
@@ -34,14 +33,19 @@ auto main() -> int {
 
     camera->transform.Translate({0.0f, 0.0f, 2.0f});
 
-    auto flat_material = PhongMaterial::Create();
+    auto flat_material = FlatMaterial::Create();
     auto flat_mesh = Mesh::Create(BoxGeometry::Create({}), flat_material);
-    flat_material->color = 0xFF0000; // Moonstone
+
+    flat_material->texture_map = Texture2D::Create("assets/checker.png");
+    flat_mesh->transform.Translate({0.6f, 0.0f, 0.0f});
+    flat_mesh->transform.Scale(0.7f);
 
     auto shiny_material = PhongMaterial::Create();
     auto shiny_mesh = Mesh::Create(BoxGeometry::Create({}), shiny_material);
+
     shiny_material->color = 0x47A8BD; // Moonstone
-    // shiny_material->polygon_offset = {.factor = -0.1f, .units = -0.1f};
+    shiny_mesh->transform.Translate({-0.6f, 0.0f, 0.0f});
+    shiny_mesh->transform.Scale(0.7f);
 
     scene->Add(camera_controls);
     scene->Add(flat_mesh);
@@ -57,7 +61,7 @@ auto main() -> int {
         scene->ProcessUpdates(delta);
 
         flat_mesh->transform.Rotate(Vector3::Up(), 0.01f);
-        shiny_mesh->transform.Rotate(Vector3::Up(), 0.01f);
+        shiny_mesh->transform.Rotate(Vector3::Right(), 0.01f);
 
         renderer.Render(scene.get(), camera.get());
     });
