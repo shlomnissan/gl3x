3d3e5006b184f4b6cd6f4a0736a49d2537d940e0
Shlomi Nissan <admin@betamark.com>
2024-11-20 11:25:53 -0800

    Resolve ImGui header inclusion

--- external/imgui/imgui_impl_opengl3.cpp
+++ external/imgui/imgui_impl_opengl3.cpp
@@ -151,7 +151,7 @@
 #if (defined(__APPLE__) && (TARGET_OS_IOS || TARGET_OS_TV))
 #include <OpenGLES/ES3/gl.h>    // Use GL ES 3
 #else
-#include <GLES3/gl3.h>          // Use GL ES 3
+#include <glad/glad.h>
 #endif
 #elif !defined(IMGUI_IMPL_OPENGL_LOADER_CUSTOM)
 // Modern desktop OpenGL doesn't have a standard portable header file to load OpenGL function pointers.
