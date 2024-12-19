699597d7aa843b4948aea1d93bcc210dc5cb5db9
Shlomi Nissan <admin@betamark.com>
2024-12-19 06:22:41 -0800

    Add tests to the Transform2D class

---
+++ tests/math/transform_2d_test.cpp
@@ -0,0 +1,72 @@
+// Copyright 2024 Betamark Pty Ltd. All rights reserved.
+// Author: Shlomi Nissan (shlomi@betamark.com)
+
+#include <gtest/gtest.h>
+#include <test_helpers.hpp>
+
+#include <engine/math/transform_2d.hpp>
+#include <engine/math/utilities.hpp>
+
+#include <cmath>
+
+#pragma region Transformations
+
+TEST(Transform2D, Translate) {
+    auto t = engine::Transform2D {};
+    t.Translate({2.0f, 1.0f});
+
+    EXPECT_VEC2_EQ(t.GetPosition(), {2.0f, 1.0f});
+    EXPECT_MAT3_EQ(t.Get(), {
+        1.0f, 0.0f, 2.0f,
+        0.0f, 1.0f, 1.0f,
+        0.0f, 0.0f, 1.0f
+    });
+}
+
+TEST(Transform2D, Scale) {
+    auto t = engine::Transform2D {};
+    t.Scale({2.0f, 1.0f});
+
+    EXPECT_VEC2_EQ(t.GetScale(), {2.0f, 1.0f});
+    EXPECT_MAT3_EQ(t.Get(), {
+        2.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f
+    });
+}
+
+TEST(Transform2D, Rotate) {
+    auto t = engine::Transform2D {};
+    t.Rotate(engine::math::half_pi);
+
+    auto c = std::cos(t.GetRotation());
+    auto s = std::sin(t.GetRotation());
+    EXPECT_EQ(t.GetRotation(), engine::math::half_pi);
+    EXPECT_MAT3_EQ(t.Get(), {
+        c, s, 0.0f,
+        -s, c, 0.0f,
+        0.0f, 0.0f, 1.0f
+    });
+}
+
+TEST(Transform2D, TransformationsWithOffset) {
+    auto t = engine::Transform2D {};
+    t.SetCenter({0.5, 0.5f});
+    t.Translate({2.0f, 3.0f});
+    t.Scale({2.0f, 2.0f});
+    t.Rotate(engine::math::half_pi);
+
+    auto c = std::cos(t.GetRotation()) * t.GetScale().x;
+    auto s = std::sin(t.GetRotation()) * t.GetScale().y;
+    EXPECT_VEC2_EQ(t.GetCenter(), {0.5f, 0.5f});
+    EXPECT_VEC2_EQ(t.GetPosition(), {2.0f, 3.0f});
+    EXPECT_VEC2_EQ(t.GetScale(), {2.0f, 2.0f});
+    EXPECT_EQ(t.GetRotation(), engine::math::half_pi);
+    EXPECT_MAT3_EQ(t.Get(), {
+        c, s, 1.5f,
+        -s, c, 4.5f,
+        0.0f, 0.0f, 1.0f
+    });
+}
+
+#pragma endregion
