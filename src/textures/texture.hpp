4639d4779709669834144f956a38be192dfe281e
Shlomi Nissan <admin@betamark.com>
2024-12-19 14:17:19 -0800

    Add transformation methods to the texture for convenience

--- src/textures/texture.hpp
+++ src/textures/texture.hpp
@@ -10,9 +10,38 @@
 
 namespace engine {
 
+/**
+ * @brief Texutre base class.
+ */
 class Texture : public Disposable, public Identity {
 public:
+    /// @brief The UV transformation matrix.
     Transform2D transform;
+
+    /**
+     * @brief Set the offset of the texture on the X-axis.
+     */
+    auto OffsetX(float value) { transform.Translate({value, 0.0f}); }
+
+    /**
+     * @brief Set the offset of the texture on the Y-axis.
+     */
+    auto OffsetY(float value) { transform.Translate({0.0f, value}); }
+
+    /**
+     * @brief Set the uniform scale of the texture.
+     */
+    auto Scale(float value) { transform.Scale({value, value}); }
+
+    /**
+     * @brief Set the rotation angle of the texture.
+     */
+    auto Rotate(float angle) { transform.Rotate(angle); }
+
+    /**
+     * @brief Set the center of UV transformation.
+     */
+    auto SetCenter(float x, float y) { transform.SetCenter({x, y}); }
 };
 
 }
