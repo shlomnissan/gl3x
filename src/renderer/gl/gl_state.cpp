a55eed282816b328f9667e0967041142d8f6343a
Shlomi Nissan <admin@betamark.com>
2025-01-06 11:45:06 -0800

    Add a GLState class placeholder

---
+++ src/renderer/gl/gl_state.cpp
@@ -0,0 +1,4 @@
+// Copyright © 2024 - Present, Shlomi Nissan.
+// All rights reserved.
+
+#include "renderer/gl/gl_state.hpp"
---
+++ src/renderer/gl/gl_state.hpp
@@ -0,0 +1,10 @@
+// Copyright © 2024 - Present, Shlomi Nissan.
+// All rights reserved.
+
+namespace engine {
+
+class GLState {
+
+};
+
+}
