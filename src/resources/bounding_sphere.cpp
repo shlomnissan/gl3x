f4241290af25789037c92799ed8091126d1e47f6
Shlomi Nissan <admin@betamark.com>
2025-02-21 15:18:17 -0800

    Add XZ/YZ planes to the bounding sphere resource

--- src/resources/bounding_sphere.cpp
+++ src/resources/bounding_sphere.cpp
@@ -21,19 +21,38 @@ auto BoundingSphere::CreateGeometry(const Sphere& sphere) const -> std::shared_p
     std::vector<float> vertices;
     std::vector<unsigned> indices;
 
-    for (auto i = 0; i <= static_cast<unsigned>(segments); ++i) {
-        auto p = sphere.Center();
-        auto theta = (static_cast<float>(i) / segments) * math::two_pi;
-        p.x += sphere.Radius() * std::cos(theta);
-        p.z += sphere.Radius() * std::sin(theta);
+    const auto center = sphere.Center();
+    const auto radius = sphere.Radius();
 
-        vertices.emplace_back(p.x);
-        vertices.emplace_back(p.y);
-        vertices.emplace_back(p.z);
+    enum class Axis { XY, XZ, YZ };
 
-        if (i > 0) {
-            indices.emplace_back(i - 1);
-            indices.emplace_back(i);
+    for (auto axis = 0; axis < 3; ++axis) {
+        auto offset = vertices.size() / 3;
+        for (auto i = 0; i <= static_cast<unsigned>(segments); ++i) {
+            auto p = center;
+            auto theta = (static_cast<float>(i) / segments) * math::two_pi;
+
+            if (axis == static_cast<unsigned>(Axis::XY)) {
+                p.x += radius * std::cos(theta);
+                p.y += radius * std::sin(theta);
+            }
+            if (axis == static_cast<unsigned>(Axis::XZ)) {
+                p.x += radius * std::cos(theta);
+                p.z += radius * std::sin(theta);
+            }
+            if (axis == static_cast<unsigned>(Axis::YZ)) {
+                p.y += radius * std::cos(theta);
+                p.z += radius * std::sin(theta);
+            }
+
+            vertices.emplace_back(p.x);
+            vertices.emplace_back(p.y);
+            vertices.emplace_back(p.z);
+
+            if (i > 0) {
+                indices.emplace_back(offset + i - 1);
+                indices.emplace_back(offset + i);
+            }
         }
     }
 
