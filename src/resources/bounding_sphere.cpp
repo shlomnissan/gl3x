6d0f6bd63dcca45686af352c61930c3f32c86547
Shlomi Nissan <admin@betamark.com>
2025-02-21 11:58:10 -0800

    Convert bounding sphere to lines

--- src/resources/bounding_sphere.cpp
+++ src/resources/bounding_sphere.cpp
@@ -3,20 +3,48 @@
 
 #include "engine/resources/bounding_sphere.hpp"
 
+#include "engine/math/utilities.hpp"
 #include "engine/materials/flat_material.hpp"
 #include "engine/geometries/sphere_geometry.hpp"
+
+#include <cmath>
 
 namespace engine {
 
 BoundingSphere::BoundingSphere(const Sphere& sphere, const Color& color) {
-    auto material = FlatMaterial::Create(color);
-    material->transparent = true;
-    material->opacity = 0.5f;
-    Add(Mesh::Create(CreateGeometry(sphere), material));
+    Add(Mesh::Create(CreateGeometry(sphere), FlatMaterial::Create(color)));
 }
 
 auto BoundingSphere::CreateGeometry(const Sphere& sphere) const -> std::shared_ptr<Geometry> {
-    return SphereGeometry::Create({sphere.Radius()});
+    constexpr auto segments = 64.0f;
+
+    std::vector<float> vertices;
+    std::vector<unsigned> indices;
+
+    for (auto i = 0; i <= static_cast<unsigned>(segments); ++i) {
+        auto p = sphere.Center();
+        auto theta = (static_cast<float>(i) / segments) * math::two_pi;
+        p.x += sphere.Radius() * std::cos(theta);
+        p.z += sphere.Radius() * std::sin(theta);
+
+        vertices.emplace_back(p.x);
+        vertices.emplace_back(p.y);
+        vertices.emplace_back(p.z);
+
+        if (i > 0) {
+            indices.emplace_back(i - 1);
+            indices.emplace_back(i);
+        }
+    }
+
+    auto geometry = Geometry::Create(vertices, indices);
+    geometry->primitive = GeometryPrimitiveType::Lines;
+    geometry->SetAttribute({
+        .type = GeometryAttributeType::Position,
+        .item_size = 3
+    });
+
+    return geometry;
 }
 
 }
