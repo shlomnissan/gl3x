a48c3f5952c7a37637a4e5ede2aa804b26d9b8f7
Shlomi Nissan <shlomi@betamark.com>
2025-02-22 12:13:27 -0800

    Add tests for sphere transformations

--- tests/math/sphere_test.cpp
+++ tests/math/sphere_test.cpp
@@ -4,6 +4,7 @@
 #include <gtest/gtest.h>
 #include <test_helpers.hpp>
 
+#include <engine/math/matrix4.hpp>
 #include <engine/math/sphere.hpp>
 #include <engine/math/vector3.hpp>
 
@@ -93,6 +94,67 @@ TEST(Sphere, ExpandWithPointOutsideSphere) {
 
     EXPECT_VEC3_EQ(sphere.Center(), {0.5f, 0.0f, 0.0f});
     EXPECT_FLOAT_EQ(sphere.Radius(), 1.5f);
+}
+
+#pragma endregion
+
+#pragma region Apply Transform
+
+TEST(Sphere, TransformWithIdentityMatrix) {
+    auto sphere = engine::Sphere {{1.0f, 2.0f, 3.0f}, 4.0f};
+    const auto transform = engine::Matrix4::Identity();
+
+    sphere.ApplyTransform(transform);
+
+    EXPECT_VEC3_EQ(sphere.Center(), {1.0f, 2.0f, 3.0f});
+    EXPECT_FLOAT_EQ(sphere.Radius(), 4.0f);
+}
+
+TEST(Sphere, TransformWithTranslation) {
+    auto sphere = engine::Sphere {{1.0f, 2.0f, 3.0f}, 4.0f};
+    const auto transform = engine::Matrix4 {
+        1.0f, 0.0f, 0.0f, 2.0f,
+        0.0f, 1.0f, 0.0f, 3.0f,
+        0.0f, 0.0f, 1.0f, 4.0f,
+        0.0f, 0.0f, 0.0f, 1.0f
+    };
+
+    sphere.ApplyTransform(transform);
+
+    EXPECT_VEC3_EQ(sphere.Center(), {3.0f, 5.0f, 7.0f});
+    EXPECT_FLOAT_EQ(sphere.Radius(), 4.0f);
+}
+
+TEST(Sphere, TransformWithScale) {
+    auto sphere = engine::Sphere {{1.0f, 2.0f, 3.0f}, 4.0f};
+    const auto transform = engine::Matrix4 {
+        2.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 2.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 2.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f
+    };
+
+    sphere.ApplyTransform(transform);
+
+    EXPECT_VEC3_EQ(sphere.Center(), {2.0f, 4.0f, 6.0f});
+    EXPECT_FLOAT_EQ(sphere.Radius(), 8.0f);
+}
+
+TEST(Sphere, TransformWithRotation) {
+    auto sphere = engine::Sphere {{1.0f, 0.0f, 0.0f}, 4.0f};
+
+    // Rotate 90 degrees around the z-axis
+    const auto transform = engine::Matrix4 {
+        0.0f, -1.0f, 0.0f, 0.0f,
+        1.0f,  0.0f, 0.0f, 0.0f,
+        0.0f,  0.0f, 1.0f, 0.0f,
+        0.0f,  0.0f, 0.0f, 1.0f
+    };
+
+    sphere.ApplyTransform(transform);
+
+    EXPECT_VEC3_EQ(sphere.Center(), {0.0f, 1.0f, 0.0f});
+    EXPECT_FLOAT_EQ(sphere.Radius(), 4.0f);
 }
 
 #pragma endregion
