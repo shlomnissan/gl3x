69078b4bf174460645d6d7c125dabeff443b65cf
Shlomi Nissan <shlomi@betamark.com>
2025-03-03 07:58:20 -0800

    Add placeholder methods for the bounding plane

--- src/resources/bounding_plane.cpp
+++ src/resources/bounding_plane.cpp
@@ -3,8 +3,23 @@
 
 #include "engine/resources/bounding_plane.hpp"
 
+#include "engine/materials/flat_material.hpp"
+#include "engine/nodes/mesh.hpp"
+
+#include <vector>
+
 namespace engine {
 
-// TODO: implement BoundingPlane
+BoundingPlane::BoundingPlane(const Plane& plane, float size, const Color& color) {
+    Add(Mesh::Create(CreateGeometry(plane, size), FlatMaterial::Create(color)));
+}
+
+auto BoundingPlane::CreateGeometry(
+    const Plane& plane,
+    float size
+) const -> std::shared_ptr<Geometry> {
+    // TODO: generate geometry
+    return std::make_shared<Geometry>();
+}
 
 }
