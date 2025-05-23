c657e85cda41a1809a164b1276bc2a5cbd8d13b8
Shlomi Nissan <admin@betamark.com>
2025-05-23 07:02:03 -0700

    Add an asset compiler cmake project

---
+++ tools/asset_compiler/src/main.cpp
@@ -0,0 +1,10 @@
+// Copyright © 2024 - Present, Shlomi Nissan.
+// All rights reserved.
+
+#include <print>
+
+auto main() -> int {
+    std::print("Asset compiler is running!");
+
+    return 0;
+}
