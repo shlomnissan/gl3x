8bb720b97f6254acbac959462d4247018cf919f8
Shlomi Nissan <admin@betamark.com>
2025-05-25 12:08:13 -0700

    Parse multiple meshes and write .msh file

--- tools/asset_builder/include/types.hpp
+++ tools/asset_builder/include/types.hpp
@@ -5,12 +5,12 @@
 
 #include <cstdint>
 
-enum class TextureFormat : uint32_t {
+enum TextureFormat : uint32_t {
     RGBA8 = 0
 };
 
-enum class VertexAttributeFlags : uint32_t {
-    Position = 1 << 0,
+enum VertexAttributeFlags : uint32_t {
+    Positions = 1 << 0,
     Normals = 1 << 1,
     UVs = 1 << 2,
 };
@@ -30,7 +30,7 @@ struct TextureHeader {
 
 #pragma pack(push, 1)
 struct MeshHeader {
-    char magic[4];
+    char magic[4] = {};
     uint32_t version;
     uint32_t header_size;
     uint32_t mesh_count;
@@ -39,13 +39,13 @@ struct MeshHeader {
 
 #pragma pack(push, 1)
 struct MeshEntryHeader {
-    char name[64];
+    char name[64] = {};
     uint32_t vertex_count;
     uint32_t index_count;
     uint32_t vertex_stride;
     uint32_t material_index;
-    uint32_t vertex_flags;
     uint64_t vertex_data_size;
     uint64_t index_data_size;
+    uint32_t vertex_flags;
 };
 #pragma pack(pop)
--- tools/asset_builder/src/mesh_converter.cpp
+++ tools/asset_builder/src/mesh_converter.cpp
@@ -8,9 +8,39 @@
 
 #include <format>
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 #include "tiny_obj_loader.hpp"
+
+struct VertexKey {
+    int pos_idx;
+    int norm_idx;
+    int uv_idx;
+    auto operator<=>(const VertexKey&) const = default;
+};
+
+struct VertexKeyHash {
+    std::size_t operator()(const VertexKey& key) const {
+        return std::hash<int>()(key.pos_idx) ^
+               std::hash<int>()(key.norm_idx << 1) ^
+               std::hash<int>()(key.uv_idx << 2);
+    }
+};
+
+using VertexMap = std::unordered_map<VertexKey, unsigned, VertexKeyHash>;
+
+namespace {
+
+auto stride(const tinyobj::attrib_t& attrib) {
+    auto stride = 6u; // positions and normals are guaranteed
+    if (!attrib.texcoords.empty()) {
+        stride += 2;
+    }
+    return stride;
+}
+
+}
 
 auto convert_mesh(
     const fs::path& input_path,
@@ -44,6 +74,69 @@ auto convert_mesh(
     }
 
     out_stream.write(reinterpret_cast<const char*>(&header), sizeof(header));
+
+    for (const auto& shape : shapes) {
+        auto& mesh = shape.mesh;
+        auto seen_vertices = VertexMap {};
+        auto vertex_data = std::vector<float> {};
+        auto index_data = std::vector<unsigned> {};
+
+        for (auto i = 0u; i < mesh.indices.size(); ++i) {
+            const auto idx = mesh.indices[i];
+            VertexKey key = {idx.vertex_index, idx.normal_index, idx.texcoord_index};
+
+            if (seen_vertices.contains(key)) {
+                index_data.push_back(seen_vertices[key]);
+                continue;
+            }
+
+            seen_vertices[key] = static_cast<unsigned>(vertex_data.size() / stride(attrib));
+            vertex_data.insert(vertex_data.end(), {
+                attrib.vertices[3 * idx.vertex_index + 0],
+                attrib.vertices[3 * idx.vertex_index + 1],
+                attrib.vertices[3 * idx.vertex_index + 2]
+            });
+
+            if (idx.normal_index >= 0) {
+                vertex_data.insert(vertex_data.end(), {
+                    attrib.normals[3 * idx.normal_index + 0],
+                    attrib.normals[3 * idx.normal_index + 1],
+                    attrib.normals[3 * idx.normal_index + 2]
+                });
+            } else {
+                // if no normals are provided, insert a placeholder.
+                // these temporary values will be replaced in post-processing.
+                vertex_data.insert(vertex_data.end(), {0.0f, 0.0f, 0.0f});
+            }
+
+            if (idx.texcoord_index >= 0) {
+                vertex_data.insert(vertex_data.end(), {
+                    attrib.texcoords[2 * idx.texcoord_index + 0],
+                    attrib.texcoords[2 * idx.texcoord_index + 1]
+                });
+            }
+        }
+
+        // TODO: generate normals if not provided
+
+        auto mesh_entry_header = MeshEntryHeader {};
+        auto name = shape.name.empty() ? "default:Mesh" : shape.name;
+        std::strncpy(mesh_entry_header.name, name.c_str(), sizeof(mesh_entry_header.name) - 1);
+        mesh_entry_header.vertex_count = static_cast<uint32_t>(seen_vertices.size());
+        mesh_entry_header.index_count = static_cast<uint32_t>(index_data.size());
+        mesh_entry_header.vertex_stride = stride(attrib);
+        mesh_entry_header.material_index = 0; // TODO: handle materials
+        mesh_entry_header.vertex_data_size = static_cast<uint64_t>(vertex_data.size() * sizeof(float));
+        mesh_entry_header.index_data_size = static_cast<uint64_t>(index_data.size() * sizeof(unsigned));
+        mesh_entry_header.vertex_flags = VertexAttributeFlags::Positions | VertexAttributeFlags::Normals;
+        if (!attrib.texcoords.empty()) {
+            mesh_entry_header.vertex_flags |= VertexAttributeFlags::UVs;
+        }
+
+        out_stream.write(reinterpret_cast<const char*>(&mesh_entry_header), sizeof(mesh_entry_header));
+        out_stream.write(reinterpret_cast<const char*>(vertex_data.data()), vertex_data.size() * sizeof(float));
+        out_stream.write(reinterpret_cast<const char*>(index_data.data()), index_data.size() * sizeof(unsigned));
+    }
 
     return {};
 }
