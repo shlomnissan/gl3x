1d2b8397f37830a139604ddd79b0bb58fa827004
Shlomi Nissan <admin@betamark.com>
2025-06-04 07:25:03 -0700

    Add a scoped timer utility class for benchmarking

---
+++ src/utilities/scoped_timer.hpp
@@ -0,0 +1,56 @@
+/*
+===========================================================================
+GLEAM ENGINE https://gleamengine.org
+Copyright © 2024 - Present, Shlomi Nissan
+===========================================================================
+*/
+
+#pragma once
+
+#include "utilities/logger.hpp"
+
+#include <string>
+#include <chrono>
+
+namespace gleam {
+
+class ScopedTimer {
+public:
+    enum class Unit {
+        Microseconds,
+        Milliseconds
+    };
+
+    using Clock = std::chrono::high_resolution_clock;
+
+    ScopedTimer(const std::string& label, Unit unit = Unit::Microseconds) :
+        label_(label), unit_(unit) {}
+
+    auto Stop() {
+        if (is_running_) {
+            is_running_ = false;
+            const auto end = Clock::now();
+            const auto delta = end - start_;
+            if (unit_ == Unit::Microseconds) {
+                const auto duration = std::chrono::duration_cast<std::chrono::microseconds>(delta);
+                Logger::Log(LogLevel::Debug, "{}: {} µs", label_, duration.count());
+            }
+            if (unit_ == Unit::Milliseconds) {
+                const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(delta);
+                Logger::Log(LogLevel::Debug, "{}: {} ms", label_, duration.count());
+            }
+        }
+    }
+
+    ~ScopedTimer() {
+        Stop();
+    }
+
+private:
+    std::string label_;
+    Unit unit_;
+    Clock::time_point start_ {Clock::now()};
+    bool is_running_ {true};
+};
+
+}
