d5e7e1810888e716141566a44f6b2708d6b5c912
Shlomi Nissan <admin@betamark.com>
2025-06-14 18:08:35 -0700

    Add a drop-in replacement to GLUniform

---
+++ src/renderer/gl/gl_uniform_xyz.hpp
@@ -0,0 +1,61 @@
+/*
+===========================================================================
+GLEAM ENGINE https://gleamengine.org
+Copyright © 2024 - Present, Shlomi Nissan
+===========================================================================
+*/
+
+#pragma once
+
+#include "gleam/math/color.hpp"
+#include "gleam/math/matrix3.hpp"
+#include "gleam/math/matrix4.hpp"
+#include "gleam/math/vector2.hpp"
+#include "gleam/math/vector3.hpp"
+#include "gleam/math/vector4.hpp"
+
+#include <string>
+
+#include <glad/glad.h>
+
+namespace gleam {
+
+enum class UniformType {
+    Float,
+    Int,
+    Matrix3,
+    Matrix4,
+    Vector2,
+    Vector3,
+    Vector4
+};
+
+class GLUniformXYZ {
+public:
+    GLUniformXYZ(std::string_view name, GLint location, GLenum type);
+
+    auto SetValue(const void* value) -> void;
+
+    auto UploadIfNeeded() -> void;
+
+private:
+    std::string name_;
+
+    GLint location_ {-1};
+
+    UniformType type_;
+
+    bool needs_upload_ {false};
+
+    union {
+        GLfloat f;
+        GLint   i;
+        Matrix3 m3;
+        Matrix4 m4;
+        Vector2 v2;
+        Vector3 v3;
+        Vector4 v4;
+    } data_;
+};
+
+}
