e947aa006b349b35d71bddb921446e7e5ed12245
Shlomi Nissan <admin@betamark.com>
2025-06-23 08:06:48 -0700

    Add an umbrella header to simplify onboarding

---
+++ include/gleam/gleam.hpp
@@ -0,0 +1,19 @@
+/*
+===========================================================================
+    GLEAM ENGINE DEMOS https://gleamengine.org
+    Copyright © 2024 - Present, Shlomi Nissan
+===========================================================================
+*/
+
+#pragma once
+
+#include "gleam/cameras.hpp"
+#include "gleam/core.hpp"
+#include "gleam/geometries.hpp"
+#include "gleam/lights.hpp"
+#include "gleam/loaders.hpp"
+#include "gleam/materials.hpp"
+#include "gleam/math.hpp"
+#include "gleam/nodes.hpp"
+#include "gleam/resources.hpp"
+#include "gleam/textures.hpp"
