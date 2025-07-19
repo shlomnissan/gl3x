8bcb401fcb51b92f518e72b2b3865354d001651f
Shlomi Nissan <admin@betamark.com>
2025-07-19 05:57:25 -0700

    Make Transform2 constexpr

--- include/gleam/math/transform2.hpp
+++ include/gleam/math/transform2.hpp
@@ -19,7 +19,7 @@ class GLEAM_EXPORT Transform2 {
 public:
     bool touched {true};
 
-    auto Translate(const Vector2& value) {
+    constexpr auto Translate(const Vector2& value) {
         const float s = math::Sin(rotation_);
         const float c = math::Cos(rotation_);
         const Vector2 rotated_value = {
@@ -30,53 +30,53 @@ public:
         touched = true;
     }
 
-    auto Scale(const Vector2& value) {
+    constexpr auto Scale(const Vector2& value) {
         scale_ *= value;
         touched = true;
     }
 
-    auto Rotate(float angle) {
+    constexpr auto Rotate(float angle) {
         rotation_ += angle;
         touched = true;
     }
 
-    auto SetPosition(const Vector2& position) {
+    constexpr auto SetPosition(const Vector2& position) {
         if (position_ != position) {
             position_ = position;
             touched = true;
         }
     }
 
-    auto SetScale(const Vector2& scale) {
+    constexpr auto SetScale(const Vector2& scale) {
         if (scale_ != scale) {
             scale_ = scale;
             touched = true;
         }
     }
 
-    auto SetRotation(float rotation) {
+    constexpr auto SetRotation(float rotation) {
         if (rotation_ != rotation) {
             rotation_ = rotation;
             touched = true;
         }
     }
 
-    auto SetCenter(const Vector2& center) {
+    constexpr auto SetCenter(const Vector2& center) {
         if (center_ != center) {
             center_ = center;
             touched = true;
         }
     }
 
-    [[nodiscard]] auto GetPosition() const { return position_; }
+    [[nodiscard]] constexpr auto GetPosition() const { return position_; }
 
-    [[nodiscard]] auto GetScale() const { return scale_; }
+    [[nodiscard]] constexpr auto GetScale() const { return scale_; }
 
-    [[nodiscard]] auto GetRotation() const { return rotation_; }
+    [[nodiscard]] constexpr auto GetRotation() const { return rotation_; }
 
-    [[nodiscard]] auto GetCenter() const { return center_; }
+    [[nodiscard]] constexpr auto GetCenter() const { return center_; }
 
-    [[nodiscard]] auto Get() {
+    [[nodiscard]] constexpr auto Get() {
         if (touched) {
             touched = false;
             const float rc = math::Cos(rotation_);
--- tests/math/transform2_test.cpp
+++ tests/math/transform2_test.cpp
@@ -11,45 +11,75 @@
 #include <gleam/math/transform2.hpp>
 #include <gleam/math/utilities.hpp>
 
+#include <cassert>
+
 #pragma region Mutators
 
 TEST(Transform2, SetPosition) {
-    auto t = gleam::Transform2 {};
-    t.SetPosition({2.0f, 1.0f});
+    auto t1 = gleam::Transform2 {};
+    t1.SetPosition({2.0f, 1.0f});
 
-    EXPECT_VEC2_EQ(t.GetPosition(), {2.0f, 1.0f});
-    EXPECT_MAT3_EQ(t.Get(), {
+    EXPECT_VEC2_EQ(t1.GetPosition(), {2.0f, 1.0f});
+    EXPECT_MAT3_EQ(t1.Get(), {
         1.0f, 0.0f, 2.0f,
         0.0f, 1.0f, 1.0f,
         0.0f, 0.0f, 1.0f
     });
+
+    constexpr auto t2 = []() {
+        auto t = gleam::Transform2 {};
+        t.SetPosition({2.0f, 1.0f});
+        return t;
+    }();
+
+    static_assert(t2.GetPosition().x == 2.0f);
+    static_assert(t2.GetPosition().y == 1.0f);
 }
 
 TEST(Transform2, SetScale) {
-    auto t = gleam::Transform2 {};
-    t.SetScale({2.0f, 1.0f});
+    auto t1 = gleam::Transform2 {};
+    t1.SetScale({2.0f, 1.0f});
 
-    EXPECT_VEC2_EQ(t.GetScale(), {2.0f, 1.0f});
-    EXPECT_MAT3_EQ(t.Get(), {
+    EXPECT_VEC2_EQ(t1.GetScale(), {2.0f, 1.0f});
+    EXPECT_MAT3_EQ(t1.Get(), {
         2.0f, 0.0f, 0.0f,
         0.0f, 1.0f, 0.0f,
         0.0f, 0.0f, 1.0f
     });
+
+    constexpr auto t2 = []() {
+        auto t = gleam::Transform2 {};
+        t.SetScale({2.0f, 1.0f});
+        return t;
+    }();
+
+    static_assert(t2.GetScale().x == 2.0f);
+    static_assert(t2.GetScale().y == 1.0f);
 }
 
 TEST(Transform2, SetRotation) {
     auto t = gleam::Transform2 {};
     t.SetRotation(gleam::math::pi_over_2);
 
-    auto r = t.GetRotation();
-    auto c = gleam::math::Cos(r);
-    auto s = gleam::math::Sin(r);
-    EXPECT_EQ(t.GetRotation(), r);
+    constexpr auto c = gleam::math::Cos(gleam::math::pi_over_2);
+    constexpr auto s = gleam::math::Sin(gleam::math::pi_over_2);
+
     EXPECT_MAT3_EQ(t.Get(), {
         c, -s, 0.0f,
         s, c, 0.0f,
         0.0f, 0.0f, 1.0f
     });
+
+    constexpr auto m = []() {
+        auto t = gleam::Transform2 {};
+        t.SetRotation(gleam::math::pi_over_2);
+        return t.Get();
+    }();
+
+    static_assert(m[0].x == c);
+    static_assert(m[0].y == s);
+    static_assert(m[1].x == -s);
+    static_assert(m[1].y == c);
 }
 
 TEST(Transform2, MultipleTransformations) {
@@ -59,8 +89,9 @@ TEST(Transform2, MultipleTransformations) {
     t.SetScale({2.0f, 2.0f});
     t.SetRotation(gleam::math::pi_over_2);
 
-    auto c = gleam::math::Cos(t.GetRotation()) * t.GetScale().x;
-    auto s = gleam::math::Sin(t.GetRotation()) * t.GetScale().y;
+    constexpr auto c = gleam::math::Cos(gleam::math::pi_over_2) * 2.0f;
+    constexpr auto s = gleam::math::Sin(gleam::math::pi_over_2) * 2.0f;
+
     EXPECT_VEC2_EQ(t.GetCenter(), {0.5f, 0.5f});
     EXPECT_VEC2_EQ(t.GetPosition(), {2.0f, 3.0f});
     EXPECT_VEC2_EQ(t.GetScale(), {2.0f, 2.0f});
@@ -70,6 +101,22 @@ TEST(Transform2, MultipleTransformations) {
         s, c, 2.5f,
         0.0f, 0.0f, 1.0f
     });
+
+    constexpr auto m = []() {
+        auto t = gleam::Transform2 {};
+        t.SetCenter({0.5f, 0.5f});
+        t.SetPosition({2.0f, 3.0f});
+        t.SetScale({2.0f, 2.0f});
+        t.SetRotation(gleam::math::pi_over_2);
+        return t.Get();
+    }();
+
+    static_assert(m[0].x == c);
+    static_assert(m[0].y == s);
+    static_assert(m[1].x == -s);
+    static_assert(m[1].y == c);
+    static_assert(m[2].x == 3.5f);
+    static_assert(m[2].y == 2.5f);
 }
 
 #pragma endregion
@@ -77,15 +124,24 @@ TEST(Transform2, MultipleTransformations) {
 #pragma region Cumulative Transformations
 
 TEST(Transform2, Translate) {
-    auto t = gleam::Transform2 {};
-    t.Translate({2.0f, 1.0f});
+    auto t1 = gleam::Transform2 {};
+    t1.Translate({2.0f, 1.0f});
 
-    EXPECT_VEC2_EQ(t.GetPosition(), {2.0f, 1.0f});
-    EXPECT_MAT3_EQ(t.Get(), {
+    EXPECT_VEC2_EQ(t1.GetPosition(), {2.0f, 1.0f});
+    EXPECT_MAT3_EQ(t1.Get(), {
         1.0f, 0.0f, 2.0f,
         0.0f, 1.0f, 1.0f,
         0.0f, 0.0f, 1.0f
     });
+
+    constexpr auto t2 = []() {
+        auto t = gleam::Transform2 {};
+        t.Translate({2.0f, 1.0f});
+        return t;
+    }();
+
+    static_assert(t2.GetPosition().x == 2.0f);
+    static_assert(t2.GetPosition().y == 1.0f);
 }
 
 TEST(Transform2, Scale) {
@@ -98,20 +154,40 @@ TEST(Transform2, Scale) {
         0.0f, 1.0f, 0.0f,
         0.0f, 0.0f, 1.0f
     });
+
+    constexpr auto t2 = []() {
+        auto t = gleam::Transform2 {};
+        t.Scale({2.0f, 1.0f});
+        return t;
+    }();
+
+    static_assert(t2.GetScale().x == 2.0f);
+    static_assert(t2.GetScale().y == 1.0f);
 }
 
 TEST(Transform2, Rotate) {
     auto t = gleam::Transform2 {};
     t.Rotate(gleam::math::pi_over_2);
 
-    auto c = gleam::math::Cos(t.GetRotation());
-    auto s = gleam::math::Sin(t.GetRotation());
-    EXPECT_EQ(t.GetRotation(), gleam::math::pi_over_2);
+    constexpr auto c = gleam::math::Cos(gleam::math::pi_over_2);
+    constexpr auto s = gleam::math::Sin(gleam::math::pi_over_2);
+
     EXPECT_MAT3_EQ(t.Get(), {
         c, -s, 0.0f,
         s, c, 0.0f,
         0.0f, 0.0f, 1.0f
     });
+
+    constexpr auto m = []() {
+        auto t = gleam::Transform2 {};
+        t.Rotate(gleam::math::pi_over_2);
+        return t.Get();
+    }();
+
+    static_assert(m[0].x == c);
+    static_assert(m[0].y == s);
+    static_assert(m[1].x == -s);
+    static_assert(m[1].y == c);
 }
 
 TEST(Transform2, TransformationsWithOffset) {
@@ -121,8 +197,9 @@ TEST(Transform2, TransformationsWithOffset) {
     t.Scale({2.0f, 2.0f});
     t.Rotate(gleam::math::pi_over_2);
 
-    auto c = gleam::math::Cos(t.GetRotation()) * t.GetScale().x;
-    auto s = gleam::math::Sin(t.GetRotation()) * t.GetScale().y;
+    constexpr auto c = gleam::math::Cos(gleam::math::pi_over_2) * 2.0f;
+    constexpr auto s = gleam::math::Sin(gleam::math::pi_over_2) * 2.0f;
+
     EXPECT_VEC2_EQ(t.GetCenter(), {0.5f, 0.5f});
     EXPECT_VEC2_EQ(t.GetPosition(), {2.0f, 3.0f});
     EXPECT_VEC2_EQ(t.GetScale(), {2.0f, 2.0f});
@@ -132,6 +209,22 @@ TEST(Transform2, TransformationsWithOffset) {
         s, c, 2.5f,
         0.0f, 0.0f, 1.0f
     });
+
+    constexpr auto m = []() {
+        auto t = gleam::Transform2 {};
+        t.SetCenter({0.5f, 0.5f});
+        t.SetPosition({2.0f, 3.0f});
+        t.SetScale({2.0f, 2.0f});
+        t.SetRotation(gleam::math::pi_over_2);
+        return t.Get();
+    }();
+
+    static_assert(m[0].x == c);
+    static_assert(m[0].y == s);
+    static_assert(m[1].x == -s);
+    static_assert(m[1].y == c);
+    static_assert(m[2].x == 3.5f);
+    static_assert(m[2].y == 2.5f);
 }
 
 #pragma endregion
@@ -143,14 +236,29 @@ TEST(Transform2, TranslateBeforeRotation) {
     t.Translate({0.0f, 1.0f});
     t.Rotate(gleam::math::pi_over_2);
 
-    auto c = gleam::math::Cos(t.GetRotation());
-    auto s = gleam::math::Sin(t.GetRotation());
+    constexpr auto c = gleam::math::Cos(gleam::math::pi_over_2);
+    constexpr auto s = gleam::math::Sin(gleam::math::pi_over_2);
+
     EXPECT_VEC2_EQ(t.GetPosition(), {0.0f, 1.0f});
     EXPECT_MAT3_EQ(t.Get(), {
         c, -s, 0.0f,
         s,  c, 1.0f,
         0.0f, 0.0f, 1.0f
     });
+
+    constexpr auto m = []() {
+        auto t = gleam::Transform2 {};
+        t.Translate({0.0f, 1.0f});
+        t.Rotate(gleam::math::pi_over_2);
+        return t.Get();
+    }();
+
+    static_assert(m[0].x == c);
+    static_assert(m[0].y == s);
+    static_assert(m[1].x == -s);
+    static_assert(m[1].y == c);
+    static_assert(m[2].x == 0.0f);
+    static_assert(m[2].y == 1.0f);
 }
 
 TEST(Transform2, TranslateAfterRotation) {
@@ -158,14 +266,29 @@ TEST(Transform2, TranslateAfterRotation) {
     t.Rotate(gleam::math::pi_over_2);
     t.Translate({0.0f, 1.0f});
 
-    auto c = gleam::math::Cos(t.GetRotation());
-    auto s = gleam::math::Sin(t.GetRotation());
+    constexpr auto c = gleam::math::Cos(gleam::math::pi_over_2);
+    constexpr auto s = gleam::math::Sin(gleam::math::pi_over_2);
+
     EXPECT_VEC2_NEAR(t.GetPosition(), {-1.0f, 0.0f}, 0.001f);
-    EXPECT_MAT3_NEAR(t.Get(), {
+    EXPECT_MAT3_EQ(t.Get(), {
         c, -s, -1.0f,
         s,  c,  0.0f,
         0.0f, 0.0f, 1.0f
-    }, 0.0001f);
+    });
+
+    constexpr auto m = []() {
+        auto t = gleam::Transform2 {};
+        t.Rotate(gleam::math::pi_over_2);
+        t.Translate({0.0f, 1.0f});
+        return t.Get();
+    }();
+
+    static_assert(m[0].x == c);
+    static_assert(m[0].y == s);
+    static_assert(m[1].x == -s);
+    static_assert(m[1].y == c);
+    static_assert(m[2].x == -1.0f);
+    static_assert(m[2].y == 0.0f);
 }
 
 #pragma endregion
