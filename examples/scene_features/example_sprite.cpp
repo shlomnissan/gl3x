88e0d720c82b4f2d40d29746f4ba60dd0ba98f8b
Shlomi Nissan <admin@betamark.com>
2025-09-05 09:17:49 -0700

    Resolve crash in sprite example

--- examples/scene_features/example_sprite.cpp
+++ examples/scene_features/example_sprite.cpp
@@ -8,18 +8,10 @@
 #include "example_sprite.hpp"
 
 #include "gleam/nodes/orbit_controls.hpp"
-#include "gleam/nodes/sprite.hpp"
 
 #include <print>
-#include <memory>
 
 using namespace gleam;
-
-namespace {
-
-std::shared_ptr<Sprite> sprite {nullptr};
-
-}
 
 ExampleSprite::ExampleSprite() {
     show_context_menu_ = false;
@@ -43,10 +35,10 @@ auto ExampleSprite::OnAttached(gleam::SharedContext* context) -> void {
             if (result) {
                 auto material = SpriteMaterial::Create();
                 material->albedo_map = result.value();
-                sprite = Sprite::Create(material);
-                sprite->SetScale(0.15f);
-                sprite->TranslateY(1.0f);
-                Add(sprite);
+                sprite_ = Sprite::Create(material);
+                sprite_->SetScale(0.15f);
+                sprite_->TranslateY(1.0f);
+                Add(sprite_);
             } else {
                 std::println(stderr, "{}", result.error());
             }
@@ -55,7 +47,7 @@ auto ExampleSprite::OnAttached(gleam::SharedContext* context) -> void {
 }
 
 auto ExampleSprite::OnUpdate(float delta) -> void {
-    if (sprite) {
-        sprite->rotation += 1.0f * delta;
+    if (sprite_) {
+        sprite_->rotation += 1.0f * delta;
     }
 }
