fde34ba4ac16995d1959529356df9654ed1c6c73
Shlomi Nissan <admin@betamark.com>
2025-10-11 04:35:56 -0700

    Fix param description capitalization

--- tools/doxy_press/class_loader.py
+++ tools/doxy_press/class_loader.py
@@ -147,7 +147,7 @@ def _param_briefs_map(m: ET.Element) -> Dict[str, str]:
 
         for name in names:
             if name:
-                out[name] = brief[0].lower() + brief[1:]
+                out[name] = brief
 
     return out
 
